c3b54c439d06e267d1f8fefaab91843d30ede9b2
ondranedo <nedojedly.o@gmail.com>
2022-04-24 23:54:42 +0200

    CLEANING AND CLEARING BRANCHES 🛠️

--- DataStructure/Main.cpp
+++ DataStructure/Main.cpp
@@ -4,16 +4,6 @@
 
 int main(void)
 {
-	ds::Stack a;
-	size_t size;
-	std::cout << "Zadejte velikost staku: ";
-	std::cin >> size;
 
-	for (int i = 0; i < size; i++)
-		a.push(i+1);
-	a.printStack();
-	P(a.getMaxSize());
-	P(a.getCurrentSize());
 	system("pause>nul");
-
 }
