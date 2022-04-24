944593176c7cc41deddad7e8ddc07a8516e4c3cf
ondranedo <nedojedly.o@gmail.com>
2022-04-24 23:43:25 +0200

    *stack init *LinkedList.h uprava enum classes;presun do SindleLinkedList class *prejmenovani snigle na singly *komentaru u LinkedList.h *hotova Stack class

--- DataStructure/Main.cpp
+++ DataStructure/Main.cpp
@@ -1,35 +1,19 @@
 #include "pch.h"
-#include "LinkedList.h"
 
 #define P(x) std::cout << x << std::endl;
 
 int main(void)
 {
-	ds::LinkedList ll;
-	ll.push_end(1);
-	ll.push_end(2);
-	ll.push_end(3);
-	ll.push_end(4);
-	ll.push_end(5);
-	ll.push_end(6);
-	ll.push_end(7);
-	ll.push_end(8);
-	ll.push_end(9);
-	ll.push_end(10);
-	ll.printList();
+	ds::Stack a;
+	size_t size;
+	std::cout << "Zadejte velikost staku: ";
+	std::cin >> size;
 
-	ll.pop_last();
-	ll.pop_last();
-	ll.printList();
-
-	ll.pop_index(5);
-	ll.printList();
-
-	ll.push_index(1,69);
-	ll.push_index(1,69);
-	ll.printList();
-
-	P("size: " << ll.getSize());
-
+	for (int i = 0; i < size; i++)
+		a.push(i+1);
+	a.printStack();
+	P(a.getMaxSize());
+	P(a.getCurrentSize());
 	system("pause>nul");
+
 }
--- DataStructure/pch.h
+++ DataStructure/pch.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <stack>
+
+#include "Stack.h"
+#include "LinkedList.h"
