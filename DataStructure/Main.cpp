12b81695d3259dbe873c37bd0a680e78fa53291a
ondranedo <nedojedly.o@gmail.com>
2022-05-01 20:25:55 +0200

    *revriting *deleting all classes

--- DataStructure/Main.cpp
+++ DataStructure/Main.cpp
@@ -1,27 +1,9 @@
 #include "pch.h"
-#include "Stack.h"
-#include "LinkedList.h"
 
 #define P(x) std::cout << x << std::endl;
 
 int main()
 {
-	const char* bracketso = "<{[(";
-	const char* bracketsc = ">}])";
-	size_t nofBrackets = strlen(bracketso);
-	const char* expre = "{[]<()>}{}";
-	size_t expreSize = strlen(expre);
-	bool possible = true;
-
-
-	ds::StackLinkedList <char> buffer;
-
-	for (size_t i = 0; i < expreSize; i++)
-	{	
-
-	}
-
-	P(possible);
 
 	system("pause>nul");
 }
